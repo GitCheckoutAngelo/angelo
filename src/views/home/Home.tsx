1138c75c913d778cd55719bf82fc6e6295ce3d66
GitCheckoutAngelo <angelo25.coc@gmail.com>
2024-04-25 18:28:02 +1200

    refactor: moved home styling from css to scss

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,5 +1,5 @@
 import { LegacyRef } from 'react'
-import './Home.css'
+import './Home.scss'
 
 interface HomeProps {
   sectionRef: LegacyRef<HTMLElement>,
@@ -8,10 +8,10 @@ interface HomeProps {
 const Home = ({ sectionRef }: HomeProps) => {
   return (
     <section className="home" id="home" ref={sectionRef}>
-      <div className="home-content">
+      <div className="content">
         <h1>Angelo Tangonan</h1>
         <div className="text-animate">
-          <h3>Software Engineer</h3>          
+          <h3>Software Engineer</h3>
         </div>
         <p>Step into my world of coding where I bring ideas to life. As a student eager to learn, I'm excited to share my journey and projects with you. Let's explore together!</p>
 
@@ -20,13 +20,12 @@ const Home = ({ sectionRef }: HomeProps) => {
           <a href="#contact" className="btn">Let's Chat</a>
         </div>
       </div>
-
-      <div className="home-sci">
-        <a href="https://www.linkedin.com/in/atan958/"><i className='bx bxl-linkedin'/></a>
-        <a href="https://github.com/gitCheckoutAngelo"><i className='bx bxl-github'/></a>
-        <a href="https://www.youtube.com/channel/UCkUr-Pm-3baqWrIILajSnEg"><i className='bx bxl-youtube'/></a>
+      <div className="socials">
+        <a href="https://www.linkedin.com/in/atan958/"><i className='bx bxl-linkedin' /></a>
+        <a href="https://github.com/gitCheckoutAngelo"><i className='bx bxl-github' /></a>
+        <a href="https://www.youtube.com/channel/UCkUr-Pm-3baqWrIILajSnEg"><i className='bx bxl-youtube' /></a>
       </div>
-      <div className="home-img-overlay"></div>
+      <div className="home-img-overlay overlay"/>
     </section>
   )
 }
