07f9429619990443350426d7341d7ab436157bde
GitCheckoutAngelo <angelo25.coc@gmail.com>
2024-04-26 15:49:18 +1200

    refactor: moved index css into scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 import { SnackbarProvider } from './contexts/SnackbarContext.tsx'
 import { SectionInViewProvider } from './contexts/SectionInViewContext.tsx'
 import { SectionRefProvider } from './contexts/SectionRefContext.tsx'
